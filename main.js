7b3259756123f38f23aad1e382a6e8a309910950
Ivan Herman <ivan@ivan-herman.net>
2017-03-19 16:27:46 +0100

    Staging an async/await version
    This is just for me to learn async/await, when node.js v8 comes out (that should have the feature built in)

--- main.js
+++ main.js
@@ -11,34 +11,47 @@
 /* This is just the overall driver of the script...   */
 /******************************************************/
 
-// 1. Get the overall configuration data; this is a combination of
-//    configuration files and command line arguments
-let config = {}
+// Experimenting with the async/await version, just to test.
+// To be really tested when node.js 8 comes out (planned for April '17)
+// my understanding... this is what I understand the code below
+// looks like:
+
+const generate_minutes = async (conf) => {
+	let irc_log = await io.get_irc_log(conf);
+	let minutes = convert.to_markdown(conf.input, irc_log);
+	let message = await io.output_minutes(minutes, conf);
+	console.log(message)
+}
+
+// Get the configuration and go!
 try {
-	config  = require('./conf').get_config();
+	let config = require('./conf').get_config();
+	generate_minutes(config);
 } catch(err) {
-	console.error(`Scribejs ${err}`);
+	console.error(`Scribejs error ${err}`);
 	process.exit(-1);
 }
+
+
 
 // 2. Get the IRC Log; depending on the configuration, this is
 //    either retrieved from the W3C web site or from a local file
 // Get the configuration data
-const io      = require('./io');
-const convert = require('./convert');
-io.get_irc_log(config)
-	// 3. Convert the irc log into markdown content
-	.then((irc_log) => {
-		return convert.to_markdown(config.input, irc_log);
-	})
-	// 4. Either upload the minutes to Github or dump into a local file
-	.then((minutes) => {
-		return io.output_minutes(minutes, config);
-	})
-	// 5. Just display a message for the sake of debug/feedback
-	.then((message) => {
-		console.log(message);
-	})
-	.catch((err) => {
-		console.error(`Scribejs error: ${err}`)
-	});
+// const io      = require('./io');
+// const convert = require('./convert');
+// io.get_irc_log(config)
+// 	// 3. Convert the irc log into markdown content
+// 	.then((irc_log) => {
+// 		return convert.to_markdown(config.input, irc_log);
+// 	})
+// 	// 4. Either upload the minutes to Github or dump into a local file
+// 	.then((minutes) => {
+// 		return io.output_minutes(minutes, config);
+// 	})
+// 	// 5. Just display a message for the sake of debug/feedback
+// 	.then((message) => {
+// 		console.log(message);
+// 	})
+// 	.catch((err) => {
+// 		console.error(`Scribejs error: ${err}`)
+// 	});
